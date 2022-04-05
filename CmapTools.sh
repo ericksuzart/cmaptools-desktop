6d1637a351e952fb06e764a20b207535bffa7f20
Erick Suzart Souza <ericksuzart@gmail.com>
2022-04-05 11:57:20 -0300

    Mommit

---
+++ CmapTools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# sudo cp cmaptools.desktop ~/.local/share/applications/
+# sudo chmod +x ~/.local/share/applications/cmaptools.desktop
+sudo cp cmaptools.desktop /usr/share/applications/
+sudo chmod +x /usr/share/applications/cmaptools.desktop
+
+sudo mkdir /usr/share/applications/cmaptools
+sudo cp ${HOME}/IHMC\ CmapTools/CmapTools /usr/share/applications/cmaptools
+sudo cp concept-map-cmaptools.png /usr/share/applications/cmaptools
+
