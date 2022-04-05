8a5b23258ac075cef7a9b51cbd4a1829bdae96d2
Erick Suzart Souza <ericksuzart@gmail.com>
2022-04-05 13:05:14 -0300

    End commit

--- CmapTools.sh
+++ CmapTools.sh
@@ -3,7 +3,6 @@
 sudo cp cmaptools.desktop /usr/share/applications/
 sudo chmod +x /usr/share/applications/cmaptools.desktop
 
-sudo mkdir /usr/share/applications/cmaptools
-sudo cp ${HOME}/IHMC\ CmapTools/CmapTools /usr/share/applications/cmaptools
-sudo cp concept-map-cmaptools.png /usr/share/applications/cmaptools
-
+sudo mkdir -p /usr/share/cmaptools
+sudo cp concept-map-cmaptools.png /usr/share/cmaptools
+sudo ln -s ${HOME}/IHMC\ CmapTools/CmapTools /usr/share/cmaptools/CmapTools.lnk
--- readme.md
+++ readme.md
@@ -1,7 +1,13 @@
-# How to install
+# CmapTools desktop launcher
+
+This script will install CmapTools icon on ubuntu.
+
+## How to install
 
 Download this as zip, extract, open the folder on terminal and run:
 
 ``` bash
 chmod +x CmapTools.sh && ./CmapTools.sh
 ```
+
+Then, should appear the application icon on ubuntu launcher.
